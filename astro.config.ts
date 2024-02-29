6fb44199dc3489a0615ac4e8e5424a739bfbbc65
Chase <chase@slept.dev>
2024-02-28 19:34:23 -0600

    (test) comment out baseurl

--- astro.config.ts
+++ astro.config.ts
@@ -14,7 +14,7 @@ import { expressiveCodeOptions } from "./src/site.config";
 export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
 	site: "https://slept.dev/study/",
-	base: "/study",
+	// base: "/study",
 	markdown: {
 		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
 		rehypePlugins: [
